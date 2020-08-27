f751854661e904b19ac6f0894b45629ae8de1d60
AlejandroGC7 <alejandrogac77@gmail.com>
2020-08-27 13:57:41 -0500

    Paga Trabajador

---
+++ p03_AreaTriangulo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace p03_AreaTriangulo
+{
+    class Program
+    {
+        //Este programa permite calcular el área del triángulo
+        //Alejandro García Cortés
+        static void Main(string[] args)
+        {
+            float baseT, alturaT, areaT;
+            
+            Console.WriteLine("Dame la base:"); baseT = float.Parse(Console.ReadLine());
+            Console.WriteLine("Dame la altura:"); alturaT = float.Parse(Console.ReadLine());
+
+            areaT = baseT * alturaT / 2;
+            Console.WriteLine($"Un triangulo de base {baseT} y altura {alturaT} tiene un area de {areaT}");
+        }
+    }
+}
---
+++ p04_PagaTrabajador/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+// Programa que calcula la paga de un trabajador dado, nombre, horas, paga y tasa de impuestos
+// Alejandro García Cortés
+
+namespace p04_PagaTrabajador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            int horas;
+            float paga, tasa=0.10f;
+            float impuesto, pagabruta, paganeta;
+
+            Console.WriteLine("Calculando la paga del trabajador");
+            Console.WriteLine("Dame el nombre"); nombre = Console.ReadLine();
+            Console.WriteLine("Dame el horas"); horas = int.Parse(Console.ReadLine());
+            Console.WriteLine("Dame la paga"); paga = float.Parse(Console.ReadLine());
+
+            pagabruta = horas * paga;
+            impuesto = pagabruta * tasa;
+            paganeta = pagabruta -impuesto;
+
+            Console.WriteLine($"El trabajador de nombre {nombre}");
+            Console.WriteLine($"Trabajo {horas} horas");
+            Console.WriteLine($"Con una paga de {paga} pesos");
+            Console.WriteLine($"Por lo cual recibe una paga bruta de {pagabruta} pesos");
+            Console.WriteLine($"Esto genera un impuesto de {impuesto} pesos");
+            Console.WriteLine($"Al final llega a su casa con la miserable cantidad de {paganeta} pesos");
+            
+        }
+    }
+}
