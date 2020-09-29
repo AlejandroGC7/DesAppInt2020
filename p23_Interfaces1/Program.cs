a0979fcfef9d610bbf182b2079631e044a760a33
AlejandroGC7 <alejandrogac77@gmail.com>
2020-09-29 12:57:05 -0500

    Programs C# - Interfaces

---
+++ p23_Interfaces1/Program.cs
@@ -0,0 +1,43 @@
+﻿//UNIVERSIDAD AUTÓNOMA DE ZACATECAS - INGENIERÍA EN COMPUTACIÓN.
+//Desarrollo de Aplicaciones en Internet.
+//Programa que hace uso de Interfaces(1).
+//Alejandro García Cortés.
+//29/09/2020.
+
+using System;
+
+namespace p23_Interfaces1
+{
+    class Program
+    {
+        public interface IAnimal{
+            string Nombre {get; set;}
+            void Llorar();
+        }
+
+        class Perro : IAnimal{
+            public Perro(string nombre) => Nombre = nombre;
+            public string Nombre {get; set;}
+            public void Llorar() => Console.WriteLine("Woff Woff");
+        }
+
+        class Gato : IAnimal{
+            public Gato(string nombre) => Nombre = nombre;
+            public string Nombre {get; set;}
+            public void Llorar() => Console.WriteLine("Miaw Miaw");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nPrimer ejemplo de uso de interfaces:\n");
+            
+            Perro perro = new Perro("Sabueso");
+            Console.WriteLine($"El perro {perro.Nombre}");
+            perro.Llorar();
+
+            Gato gato =new Gato("Misifous");
+            Console.WriteLine($"El gato {gato.Nombre}");
+            gato.Llorar();
+        }
+    }
+}
---
+++ p24_Interfaces2/Program.cs
@@ -0,0 +1,73 @@
+﻿//UNIVERSIDAD AUTÓNOMA DE ZACATECAS - INGENIERÍA EN COMPUTACIÓN.
+//Desarrollo de Aplicaciones en Internet.
+//Programa que hace uso de Interfaces(2).
+//Alejandro García Cortés.
+//29/09/2020.
+
+using System;
+
+namespace p24_Interfaces2
+{
+    public class Organismo{
+        public void Respiracion() => Console.WriteLine("Respiracion");
+        public void Movimiento() => Console.WriteLine("Movimiento");
+        public void Crecimiento() => Console.WriteLine("Crecimiento");
+    }
+
+    public interface IAnimales{
+        void MultiCelular();
+        void SangreCaliente();
+    }
+
+    public interface ICanino : IAnimales{
+        void Correr();
+        void CuatroPatas();
+    }
+
+    public interface IPajaro : IAnimales{
+        void Volar();
+        void DosPatas();
+    }
+
+    public class Perro : Organismo, ICanino{
+        public void MultiCelular() => Console.WriteLine("Multicelular (perro)");
+        public void SangreCaliente() => Console.WriteLine("Sangre Caliente (perro)");
+        public void Correr() => Console.WriteLine("Correr (perro)");
+        public void CuatroPatas() => Console.WriteLine("Cuatro Patas (perro)");
+    }
+
+    public class Perico : Organismo, IPajaro{
+        public void MultiCelular() => Console.WriteLine("Multicelular (perico)");
+        public void SangreCaliente() => Console.WriteLine("Sangre Caliente (perico)");
+        public void Volar() => Console.WriteLine("Volar (perico)");
+        public void DosPatas() => Console.WriteLine("Dos Patas (perico)");
+    }
+    
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nSegundo ejemplo de uso de interfaces:\n");
+
+            Perro perro = new Perro();
+            Console.WriteLine("\nCaracteristicas del Perro:\n");
+            perro.Respiracion();
+            perro.Movimiento();
+            perro.Crecimiento();
+            perro.MultiCelular();
+            perro.SangreCaliente();
+            perro.Correr();
+            perro.CuatroPatas();
+
+            Perico perico = new Perico();
+            Console.WriteLine("\nCaracteristicas del Perico:\n");
+            perico.Respiracion();
+            perico.Movimiento();
+            perico.Crecimiento();
+            perico.MultiCelular();
+            perico.SangreCaliente();
+            perico.Volar();
+            perico.DosPatas();
+        }
+    }
+}
